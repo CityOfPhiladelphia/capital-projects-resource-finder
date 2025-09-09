dd330c03a90fb54d38b655d476fe1488f8921b5b
Theron Halsey <theron.halsey@phila.gov>
2025-09-09 17:10:58 -0400

    filter panel is functional

--- src/i18n/en-us.js
+++ src/i18n/en-us.js
@@ -29,5 +29,37 @@ export default {
     },
     "section2Title": "",
   },
+  "status": {
+    "category": "Status",
+    "active": "Active Projects",
+    "archived": "Archived Projects",
+    "planning": "Planning",
+    "construction": "Construction",
+    "complete": "Complete"
+  },
+  "projectCategory": {
+    "category": "Project Category",
+    "parksRecreation": "Parks & Recreation",
+    "publicHealth": "Public Health",
+    "Human Services": "Human Services",
+    "freeLibrary": "Free Library",
+    "fireDepartment": "Fire Department",
+    "policeDepartment": "Police Department",
+    "publicProperty": "Public Property",
+    "other": "Other"
+  },
+  "councilDistrict": {
+    "category": "Council District",
+    "district1": "District 1",
+    "district2": "District 2",
+    "district3": "District 3",
+    "district4": "District 4",
+    "district5": "District 5",
+    "district6": "District 6",
+    "district7": "District 7",
+    "district8": "District 8",
+    "district9": "District 9",
+    "district10": "District 10",
+  },
   "Philadephia Parks and Recreation": "Philadephia Parks and Recreation",
 }
