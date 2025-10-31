0f89fe296ec4838af599c508c90bf790bf81bb7e
Theron Halsey <theron.halsey@phila.gov>
2025-10-31 16:15:44 -0400

    fix for sql query

--- src/data-sources/capitalProjects.js
+++ src/data-sources/capitalProjects.js
@@ -7,72 +7,43 @@ import { normalizeCategory as normalizeSiteCategory } from '../general/helperFun
 
 // gives app the best chance of displaying a site name, rather than a site name with the project description
 const getShortestSiteName = (siteNames) => {
-  let shortestName = siteNames[0].split(' - ')[0];
-  let shortestLength = shortestName.length;
-
+  let shortestLength = siteNames[0].length;
+  let shortestName = siteNames[0];
   for (let i = 1; i < siteNames.length; i++) {
-    const nextName = siteNames[i].split(' - ')[0];
-    shortestName = nextName.length < shortestName.length ? nextName : shortestName;
+    shortestName = siteNames[i].length < shortestLength ? siteNames[i] : shortestName;
   }
   return formatSiteName(shortestName);
 }
 
 const sqlQuery = `
   SELECT
-    site_code,
-    COALESCE(
-      lat,
-      (SELECT lat
-        FROM capital_projects_for_finder l1
-        WHERE sites.site_code = l1.site_code
-        AND sites.lat IS NOT NULL
-        LIMIT 1),
-      0) AS lat,
-    COALESCE(
-      lon,
-      (SELECT lon
-        FROM capital_projects_for_finder l2
-        WHERE sites.site_code = l2.site_code
-        AND sites.lon IS NOT NULL
-        LIMIT 1),
-      0) AS lon,
-    array_agg(DISTINCT site_name) FILTER (WHERE site_name IS NOT NULL) AS site_name,
-    array_agg(DISTINCT site_address) FILTER (WHERE site_address IS NOT NULL) AS site_address,
-    array_agg(DISTINCT client_category) FILTER (WHERE client_category IS NOT NULL) AS site_category,
-    array_agg(DISTINCT council_district) FILTER (WHERE council_district IS NOT NULL) AS council_district,
-    ARRAY(
-      SELECT jsonb_build_object(
-        'project_name', projects.project_name,
-        'project_category', projects.client_category,
-        'project_scope',  projects.project_scope,
-        'project_status', projects.project_status,
-        'project_estimated_cost', projects.project_estimated_cost,
-        'estimated_completion_season', projects.estimated_completion_season,
-        'estimated_completion_year', projects.estimated_completion_year,
-        'actual_completion', projects.actual_completion,
-        'archive_date',  projects.archive_date,
-        'project_coordinator', projects.project_coordinator,
-        'inspector', projects.inspector,
-        'contact_email', projects.contact_email,
-        'website_link', projects.website_link,
-        'fields_hash', projects.fields_hash)
-      FROM capital_projects_for_finder projects
-      WHERE sites.site_code = projects.site_code
-        AND sites.lat = COALESCE(
-          projects.lat,
-          (SELECT lat
-            FROM capital_projects_for_finder l1
-            WHERE projects.site_code = l1.site_code
-            AND projects.lat IS NOT NULL LIMIT 1),
-          0)
-        AND sites.lon = COALESCE(
-          projects.lon,
-          (SELECT lon
-            FROM capital_projects_for_finder l2
-            WHERE projects.site_code = l2.site_code
-            AND projects.lon IS NOT NULL LIMIT 1),
-          0)
-    ) AS projects
+  site_code,
+  COALESCE(lat, 0) AS lat,
+  COALESCE(lon, 0) AS lon,
+  array_agg(DISTINCT site_name) FILTER (WHERE site_name IS NOT NULL) AS site_name,
+  array_agg(DISTINCT site_address) FILTER (WHERE site_address IS NOT NULL) AS site_address,
+  array_agg(DISTINCT client_category) FILTER (WHERE client_category IS NOT NULL) AS site_category,
+  array_agg(DISTINCT council_district) FILTER (WHERE council_district IS NOT NULL) AS council_district,
+  ARRAY(
+    SELECT jsonb_build_object(
+      'project_name', t.project_name,
+      'project_category', t.client_category,
+      'project_scope',  t.project_scope,
+      'project_status', t.project_status,
+      'project_estimated_cost', t.project_estimated_cost,
+      'estimated_completion_season', t.estimated_completion_season,
+      'estimated_completion_year', t.estimated_completion_year,
+      'actual_completion', t.actual_completion,
+      'archive_date',  t.archive_date,
+      'project_coordinator', t.project_coordinator,
+      'inspector', t.inspector,
+      'contact_email', t.contact_email,
+      'website_link', t.website_link,
+      'fields_hash', t.fields_hash
+    )
+    FROM capital_projects_for_finder t
+    WHERE sites.site_code = t.site_code AND ((sites.lat = t.lat AND sites.lon = t.lon) OR t.lat IS NULL)
+   ) AS projects
   FROM (TABLE capital_projects_for_finder ORDER BY site_code, site_name, site_address, council_district, lat, lon) sites
   GROUP BY site_code, lat, lon
 `
@@ -90,12 +61,42 @@ export default {
     },
     success: function (data) {
 
-      data.rows.forEach((row) => {
+
+      data.rows.forEach((row, i, original) => {
+
         row.site_name = getShortestSiteName(row.site_name);
         row.site_address = Array.isArray(row.site_address) ? row.site_address[0] : row.site_address;
         row.site_category = normalizeSiteCategory(row.site_category);
         row.council_district = Array.isArray(row.council_district) ? row.council_district[0] : row.council_district;
 
+        // const seenSites = {};
+        // const iRemoved = [];
+
+        //   row.site_name = Array.isArray(row.site_name) ? row.site_name : [row.site_name];
+        //   const siteName = getShortestSiteName(row.site_name);
+
+        //   row.site_name.concat(siteName).forEach((name) => {
+        //     seenSites[name] = seenSites[name] ? Math.min(i, seenSites[name]) : i;
+        //   })
+
+        //   const j = seenSites[siteName];
+        //   if (j < i) {
+        //     original[j].site_name = getShortestSiteName([original[j].site_name, siteName]);
+        //     original[j].site_category = normalizeSiteCategory(original[j].site_category.concat(row.site_category));
+        //     original[j].projects = original[j].projects.concat(row.projects);
+        //     iRemoved.push(i);
+        //   }
+        //   else {
+        //     row.site_name = siteName;
+        //     row.site_address = Array.isArray(row.site_address) ? row.site_address[0] : row.site_address;
+        //     row.site_category = normalizeSiteCategory(row.site_category);
+        //     row.council_district = Array.isArray(row.council_district) ? row.council_district[0] : row.council_district;
+        //   }
+
+        // })
+
+        // iRemoved.forEach((i, j) => {
+        //   data.rows.splice(i - j, 1)
       })
 
       if (import.meta.env.VITE_DEBUG) console.log('capitalProjects data:', data);
