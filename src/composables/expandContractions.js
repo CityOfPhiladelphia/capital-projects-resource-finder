e30ddd6b8e508ecd28e073b9cb8c18073d5d81f3
Theron Halsey <theron.halsey@phila.gov>
2025-11-13 16:38:05 -0500

    working on fixing site names again

--- src/composables/expandContractions.js
+++ src/composables/expandContractions.js
@@ -4,6 +4,8 @@ export const expandContractions = (rawString) => {
   rawString = rawString.replace(/(?<=\W|\b)([Pp][Gg])|([Pp]\/[Gg])(?=\W|\b)/g, 'playground') // pg to playground
   rawString = rawString.replace(/(?<=\W|\b)R([Cc]|[Ee][Cc])(?=\W|\b)/g, 'recreation center') // rc or rec to recreation center
   rawString = rawString.replace(/(?<=\W|\b)[Cc]([Cc]|[Rr][Cc])(?=\W|\b)/g, 'community center') // cc or crc to community center
+  rawString = rawString.replace(/(?<=\W|\b)[Ee][Cc](?=\W|\b)/g, 'environmental center') // cc or crc to community center
+  rawString = rawString.replace(/(?<=\W|\b)[Oo][Aa][Cc](?=\W|\b)/g, 'older adult center') // cc or crc to community center
   rawString = rawString.replace(/(?<=\W|\b)[Cc][Tt][Rr](?=\W|\b)/g, 'center') // ctr to center
   rawString = rawString.replace(/(?<=\W|\b)[Hh][Oo][Rr][Tt](?=\W|\b)/g, 'horticulture') // hort to horticulture
   rawString = rawString.replace(/(?<=\W|\b)[Rr][Dd](?=\W|\b)/g, 'road') // rd to road
--- src/composables/getShortestSiteName.js
+++ src/composables/getShortestSiteName.js
@@ -3,17 +3,18 @@ import { formatString as formatSiteName } from './formatString'
 // gives app the best chance of displaying a site name, rather than a site name with the project description
 // ignores text that follows key terms to cut off project description text that is erroneously included as part of the site name
 export const getShortestSiteName = (siteNames) => {
-  let shortestName = formatSiteName(siteNames[0]).split(' - ')[0];
-  const shortestNameTrimmed = shortestName.match(/^.*?((?<=Community Center|Recreation Center|Playground)|.*?(?<=\)))/);
-  shortestName = shortestNameTrimmed ? shortestNameTrimmed[0] : shortestName
-  let shortestLength = /[a-zA-Z][/\\][a-zA-Z]/.test(shortestName) ? Infinity : shortestName.length
+  const siteNameTerminators = /^.*?((?<=Center|Playground|Library|Building|Launch|Tower|Foerd)|.*?(?<=\)))/;
+  let shortestName = formatSiteName(siteNames[0]);
+  // let shortestName = formatSiteName(siteNames[0]).split(',')[0];
+  // const shortestNameTrimmed = shortestName.match(siteNameTerminators);
+  // shortestName = shortestNameTrimmed ? shortestNameTrimmed[0] : shortestName;
 
   for (let i = 1; i < siteNames.length; i++) {
-    let nextName = formatSiteName(siteNames[i]).split(' - ')[0]
-    const nextNameTrimmed = shortestName.match(/^.*?((?<=Community Center|Recreation Center|Playground)|.*?(?<=\)))/);
-    nextName = nextNameTrimmed ? nextNameTrimmed[0] : nextName
-    const nextLength = /[a-zA-Z][/\\][a-zA-Z]/.test(nextName) ? Infinity : shortestName.length
-    shortestName = nextLength < shortestLength ? nextName : shortestName;
+    let nextName = formatSiteName(siteNames[i]);
+    // let nextName = formatSiteName(siteNames[i]).split(',')[0];
+    // const nextNameTrimmed = shortestName.match(siteNameTerminators);
+    // nextName = nextNameTrimmed ? nextNameTrimmed[0] : nextName;
+    shortestName = nextName.length < shortestName.length ? nextName : shortestName;
   }
   return shortestName
 }
